247603374f282e3ba83e7e14bb250e4b26f5bd68
Cesar <cesarcorregiari@gmail.com>
2021-04-14 18:30:45 -0300

    toast ok, just need new style

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fbd0d36052e3f00a30af";
+/******/ 	var hotCurrentHash = "34f7fb39c7863a8738f4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/pages/components/Contact.jsx
+++ src/pages/components/Contact.jsx
@@ -9,8 +9,26 @@ import whats from '../../assets/images/whats.png';
 //   return alert('Please fill all the fields');
 // };
 
-const notify = () =>
-  toast(`Information required`, {
+// const notify = () =>
+//   toast(`Information required`, {
+//     position: 'bottom-center',
+//     autoClose: 3000,
+//     hideProgressBar: false,
+//     closeOnClick: true,
+//     progress: undefined,
+//   });
+
+const messageSent = () =>
+  toast(`Thanks for your message! I'll answer soon`, {
+    position: 'bottom-center',
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    progress: undefined,
+  });
+
+const messageFailed = () =>
+  toast(`Please fill all the fields`, {
     position: 'bottom-center',
     autoClose: 3000,
     hideProgressBar: false,
@@ -23,15 +41,13 @@ const Contact = () => {
     <Formik
       initialValues={{ name: '', email: '', message: '' }}
       validationSchema={Yup.object({
-        name: Yup.string()
-          // .max(15, 'Must be 15 characters or less')
-          .required(() => notify()),
-
-        email: Yup.string()
-          .email('Invalid email address')
-          .required(() => notify()),
-
-        message: Yup.string().required(() => notify()),
+        name: Yup.string(),
+        // .max(15, 'Must be 15 characters or less')
+        // .required(),
+        email: Yup.string().email('Invalid email address'),
+        // .required()
+        message: Yup.string(),
+        // .required()
       })}
       onSubmit={(values, { setSubmitting }) => {
         console.log('sent', JSON.stringify(values, null, 2));
@@ -39,9 +55,14 @@ const Contact = () => {
           .post('/api/mail', values)
           .then(function (response) {
             console.log(response);
+            messageSent();
           })
           .catch(function (error) {
             console.log(error);
+            const { name, email, message } = values;
+            if (name || email || message === '') {
+              return messageFailed();
+            }
           });
         setSubmitting(false);
       }}
